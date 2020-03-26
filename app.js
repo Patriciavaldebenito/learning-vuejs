e539d82341790dabf139ed11d473038bf73d3f2f
Patriciavaldebenito <patricia.vpanes@gmail.com>
2020-03-26 19:51:47 -0300

    Se agrega propiedad methods en la instancia de vue en app.js, y se enlaza propiedad con valor boolean , para ejecutar v-on /mouseover, mouseout y click  en una lista e imagen

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ new Vue({
             name:'Bitcoin',
             img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=001',
             changePercent:10,
-            prices:[8400,7900,8200,9000,9400,10000],
+            // prices:[8400,7900,8200,9000,9400,10000],
             pricesWithDays:[
                 { day:'Lunes',value:8400 },
                 { day:'Martes',value:7900 },
@@ -15,8 +15,19 @@ new Vue({
                 { day:'Viernes',value:9400 },
                 { day:'Sabado',value:10000 },
                 { day:'Domingo',value:10200 }
-            ]
+            ],
+            showPrices:false //true
+        }
+    },
+    methods:{
+        toggleShowPrices(){
+            this.showPrices = !this.showPrices
         }
     }
 
 })
+
+
+
+// methods: Objeto de la instancia de Vue donde se puede definir funciones, que se pueden utilizar en diferentes contexto, principalmente para atacharse a eventos que pueden ser disparados por la vista.
+// v-on directiva que sirve para escuchar eventos del DOM, tales como onclick, onmouseover, mouseout, entre otros, para ejecutar alguna función.
